b61e467d87739b63dd40a583fdee0ac258c08b3f
Andres <andres.reinot@gmail.com>
2017-03-16 17:44:42 -0500

    no message

--- Sprite.js
+++ Sprite.js
@@ -1,4 +1,5 @@
-Sprite = function(desc) {
+var SpriteProto = Object.create(HTMLElement.prototype);
+SpriteProto.init = function(desc) {
 	//PARAMS
 	this.frameRate =  desc.frameRate  ? Math.max(0.001, desc.frameRate) : 1.0;
 	this.frameCount = desc.frameCount ? Math.max(1, desc.frameCount) : 1;
@@ -29,9 +30,11 @@ Sprite = function(desc) {
 		this.rowCount = Math.floor(this.sheet.height / this.spriteHeight);
 		document.body.appendChild(this.element);
 
-		//CALLBACK
-		this.draw(0);
-		if( this.onload ) this.onload();
+		//CALLBACK		
+		this.play();
+		this.dispatchEvent(new Event('load'));
+
+		//if( this.onload ) this.onload();
 	}.bind(this));
 
 	if( desc.url ) {
@@ -39,7 +42,7 @@ Sprite = function(desc) {
 	}
 }
 
-Sprite.prototype.step = function(timestamp) {
+SpriteProto.step = function(timestamp) {
 	var repaint = false;
 	if( !this.playTime ) {
 		if(this.loop == "once") this.rewind();
@@ -70,14 +73,21 @@ Sprite.prototype.step = function(timestamp) {
 		else {
 			paintFrame = this.playFrame % this.playRange;
 		}
-		this.draw( paintFrame + this.playOffset) ;
-		if(this.onframe) this.onframe(paintFrame + this.playOffset);
+
+		var f =  paintFrame + this.playOffset;
+		this.draw(f);
+
+		//TODO: not sure if dispatching events at 60 hz is wise		
+		var ev = new Event('frame');
+		ev.frame = f;
+		this.dispatchEvent(ev);
+		//if(this.onframe) this.onframe(paintFrame + this.playOffset);		
 	}
 	
 	this.requestID = window.requestAnimationFrame(this.step.bind(this));
 }
 
-Sprite.prototype.draw = function(frame) {
+SpriteProto.draw = function(frame) {
 	frame = frame % this.frameCount;
 	var x = frame % this.colCount;
 	var y = (frame - x) / this.colCount;
@@ -86,13 +96,13 @@ Sprite.prototype.draw = function(frame) {
 	this.context.drawImage(this.sheet, x, y, this.spriteWidth, this.spriteHeight, 0, 0, this.element.width, this.element.height);
 }
 
-Sprite.prototype.play = function(start, range) {
+SpriteProto.play = function(start, range) {
 	if( start !== undefined ) this.playOffset = start;
 	if( range !== undefined ) this.playRange = range;
 	this.step();
 }
 
-Sprite.prototype.pause = function() {
+SpriteProto.pause = function() {
 	if( this.requestID ) {
 		window.cancelAnimationFrame(this.requestID);
 		this.requestID = null;
@@ -100,15 +110,76 @@ Sprite.prototype.pause = function() {
 	}
 }
 
-Sprite.prototype.rewind = function() {
+SpriteProto.rewind = function() {
 	this.playTime = null;
 	this.playFrame = 0;
 	this.draw(0);
 }
 
-Sprite.prototype.isPlaying = function() {
+SpriteProto.isPlaying = function() {
 	return this.requestID !== null && this.requestID !== undefined;
 }
 
+SpriteProto.createdCallback = function() {}
+SpriteProto.attachedCallback = function() {
+	var desc = {
+		url: this.getAttribute("src"),
+		width: Number.parseInt(this.getAttribute("width")),
+		height: Number.parseInt(this.getAttribute("height")),
+		frameCount: Number.parseInt(this.getAttribute("frame-count")),
+		frameRate: Number.parseFloat(this.getAttribute("frame-rate")),
+		loop: this.getAttribute("loop") //once, forever, bounce
+	};
+
+	//error checking
+	var invalidErr = "Invalid <sprite-sheet> property: ";
+	if( !desc.url ) console.error(invalidErr + "src [" + desc.url + "] must be a valid image url.");
+	if( desc.width <= 0 || 		!Number.isInteger(desc.width) ) console.error(invalidErr + "width [" + desc.width + "] must be valid integer.");
+	if( desc.height <= 0 || 	!Number.isInteger(desc.height) ) console.error(invalidErr + "height [" + desc.height + "] must be valid integer.");
+	if( desc.frameCount <= 0 || !Number.isInteger(desc.frameCount) ) console.error(invalidErr + "frame-count [" + desc.frameCount + "] must be a valid integer.");
+	if( desc.frameRate <= 0 ||	!Number.isFinite(desc.frameRate) ) console.error(invalidErr + "frame-rate [" + desc.frameRate + "] must be a valid float.");
+	if( desc.loop ) {
+		desc.loop = desc.loop.toLowerCase();
+		if( desc.loop != "once" && desc.loop != "forever" && desc.loop != "bounce" ) console.error(invalidErr + "loop  [" + desc.loop + "] must be set to 'once', 'forever', or 'bounce'.");
+	}
+	
+	this.init(desc);
+	this.addEventListener("load", console.log("Loaded!"));
+	this.addEventListener("frame", console.log("Drawn!"));
+}
+SpriteProto.detachedCallback = function() {}
+
+/*
+Object.defineProperty(SpriteProto, "_width", 		{value: 256, 			writable: true});
+Object.defineProperty(SpriteProto, "_height", 		{value: 256, 			writable: true});
+Object.defineProperty(SpriteProto, "_frameCount", 	{value: 1, 				writable: true});
+Object.defineProperty(SpriteProto, "_frameRate", 	{value: 1, 				writable: true});
+Object.defineProperty(SpriteProto, "_loop", 		{value: "forever", 		writable: true});
+*/
+
+document.registerElement('sprite-sheet', {prototype: SpriteProto});
 
 
+
+/*
+	//some play pause feature cruft
+
+	var sprite = document.body.getElementsByTagName("sprite-sheet")[0];
+	sprite.addEventListener("load", function() {
+		this.element.style.border = "1px solid #0f5";
+		this.element.addEventListener("contextmenu", function(e) { e.preventDefault(); } );
+		this.element.addEventListener("mousedown", function(e) {
+			if(e.button == 0) {
+				if(this.isPlaying()) {
+					this.pause();
+					this.element.style.border = "1px solid #d00";
+				} else {
+	 				this.play();
+	 				this.element.style.border = "1px solid #0f5";
+	 			}
+			} else if( e.button == 2) {
+				this.rewind();
+			}
+		}.bind(this));
+	}.bind(sprite));
+*/
